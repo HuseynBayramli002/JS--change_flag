916ca1b18a129c5aee1c9f1183fa1563a4bb2f31
Huseynbayramli <huseynbayramli002@gmail.com>
2024-02-06 17:54:38 +0400

    regions container make

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,61 @@
-let filtRegions = document.querySelector('.header__filt-regions');
-let regionsContainer=document.querySelector('.header__regions-container');
-let modeColor=document.querySelector('.modecolor');
+let modeColor = document.querySelector('.modecolor');
+let countryContainer = document.querySelector(".main");
+let regionsContainer = document.querySelector(".header__regions__container")
 
 
-modeColor.addEventListener('click',()=>{
-    if(document.documentElement.getAttribute('data-theme')=='lightMode'){
+//! Dark thema 
+modeColor.addEventListener('click', () => {
+    if (document.documentElement.getAttribute('data-theme') == 'lightMode') {
         document.documentElement.setAttribute('data-theme', 'darkMode')
-    }else{
+    } else {
         document.documentElement.setAttribute('data-theme', 'lightMode')
     }
 })
-
-filtRegions.addEventListener("click", () => {
-    if (regionsContainer.style.display === "none") {
-        regionsContainer.style.display = "block";
-    } else {
-        regionsContainer.style.display = "none";
-    }
-
-})
-
-
-function getCountries(){
+//! Olkeleri apiden cagima
+function getCountries() {
     fetch('https://restcountries.com/v3.1/all ')
-    .then((res)=>res.json())
-    .then((data)=>{
-        countries()
-    })
+        .then((res) => res.json())
+        .then((data) => {
+            allCountries(data);
+        })
 }
 getCountries()
+//! Olkeleri elave etme
+function allCountries(countries) {
+    countryContainer.innerHTML = ''
+    countries.forEach(function (country) {
+        const { population, name, flags, region, capital } = country;
+        countryContainer.innerHTML += `
+        <div class="main__country">
+            <div class="main__flag">
+                <img src="${flags.png}" alt="">
+            </div>
+            <div class="main__country-name">
+                <h2>${name.common}</h2>
+                <h4>Population: <span>${population}</span></h4>
+                <h4>Region: <span>${region}</span></h4>
+                <h4>Capital: <span>${capital}</span></h4>
+            </div>
+        </div>
+        `;
+    });
 
-
-function allCountries(country){
-    
+}
+//! Filtere gore olkeleri filtirlemek
+regionsContainer.addEventListener("click", sellectRegion)
+function sellectRegion(e) {
+    if (e.target.value !== "") {
+        fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
+            .then((res) => res.json())
+            .then((data) => {
+                allCountries(data);
+            })
+    } else {
+        fetch(`https://restcountries.com/v3.1/all`)
+            .then((res) => res.json())
+            .then((data) => {
+                allCountries(data);
+            })
+    }
 
 }
